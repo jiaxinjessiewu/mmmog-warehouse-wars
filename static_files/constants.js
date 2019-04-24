83171efc614b943ab505132f25eeafdf82c36b58
JessieWu <jx.wu@mail.utoronto.ca>
2019-04-23 22:14:48 -0400

    install mongodb

--- static_files/constants.js
+++ static_files/constants.js
@@ -1,6 +1,6 @@
 // global.port = 11090; // CHANGE ME
-global.port = 10300; // CHANGE ME
+global.port = 8000; // CHANGE ME
 global.wwWsPort = global.port + 1; // for websockets DON'T CHANGE
-global.wwHostname = "cslinux.utm.utoronto.ca";
-// global.wwHostname = "localhost"; // CHANGE ME
+// global.wwHostname = "cslinux.utm.utoronto.ca";
+global.wwHostname = "localhost"; // CHANGE ME
 global.wwWsURL = "ws://" + global.wwHostname + ":" + global.wwWsPort; // DON'T CHANGE
